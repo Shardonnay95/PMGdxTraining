8dec8157aac1519c4c1589751196487f2cb71b03
Shardonnay95 <sharnes@pentamotorgroup.co.za>
2023-04-18 16:52:27 +0200

    Foundation Task 4

---
+++ Foundation_task_4.php
@@ -0,0 +1,26 @@
+
+<?php
+
+class ItemOwners {
+    public static function groupByOwners($ItemsArr) {
+        $OwnersItemsArr = array();
+
+        foreach ($ItemsArr as $ItemStr => $OwnerStr) {
+            if (!isset($OwnersItemsArr[$OwnerStr])) {
+                $OwnersItemsArr[$OwnerStr] = array();
+            }
+            $OwnersItemsArr[$OwnerStr][] = $ItemStr;
+        }
+
+        return $OwnersItemsArr;
+    }
+}
+
+$ItemsArr = array(
+    "Baseball Bat" => "John",
+    "Golf ball" => "Stan",
+    "Tennis Racket" => "John"
+);
+echo json_encode(ItemOwners::groupByOwners($ItemsArr));
+
+?>
