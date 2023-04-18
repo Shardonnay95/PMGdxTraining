a0ed483e8dd7428b11b9caea71d748b03ed31b3c
Shardonnay95 <sharnes@pentamotorgroup.co.za>
2023-04-18 15:56:32 +0200

    Foundation Task 2

---
+++ Foundation_task_2.php
@@ -0,0 +1,25 @@
+<?php
+
+function generateFibonacciSequence($LimitInt) {
+    $FibonacciArr = [];
+    $CurrentInt = 0;
+    $NextInt = 1;
+
+    while ($CurrentInt <= $LimitInt) {
+        array_push($FibonacciArr, $CurrentInt);
+        $TempInt = $CurrentInt + $NextInt;
+        $CurrentInt = $NextInt;
+        $NextInt = $TempInt;
+    }
+
+    return $FibonacciArr;
+}
+
+$LimitInt = 34;
+$FibonacciArr = generateFibonacciSequence($LimitInt);
+
+foreach ($FibonacciArr as $FibonacciNumberInt) {
+    echo $FibonacciNumberInt . PHP_EOL;
+}
+
+?>
