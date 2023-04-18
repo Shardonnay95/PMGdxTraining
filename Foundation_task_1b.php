4d1350054baebe8f73f39dcd1ad1948ce3a32061
Shardonnay95 <sharnes@pentamotorgroup.co.za>
2023-04-18 15:28:08 +0200

    Foundation Task 1

---
+++ Foundation_task_1b.php
@@ -0,0 +1,20 @@
+<?php
+
+// Constants
+define("SAMPLE_ARRAY_ARR", [1, 1, 1, 1, 1]);
+
+function addAll($InputArr, $TotalInt = 0) {
+    if (count($InputArr) == 0) {
+        return $TotalInt;
+    }
+
+    $TotalInt += array_sum($InputArr);
+    array_shift($InputArr);
+
+    return addAll($InputArr, $TotalInt);
+}
+
+$SampleArr = SAMPLE_ARRAY_ARR; // 5 + 4 + 3 + 2 + 1 = 15
+echo addAll($SampleArr);
+?>
+
