54efd0fa4defc7e250c7aa410b4a1a8562ab2936
Shardonnay95 <sharnes@pentamotorgroup.co.za>
2023-04-19 12:52:29 +0200

    Foundation Task 5

--- Foundation_task_5/fibonacci.php
+++ Foundation_task_5/fibonacci.php
@@ -5,8 +5,8 @@ function generateFibonacciSequence($LimitInt) {
     $NextInt = 1;
 
     while ($CurrentInt <= $LimitInt) {
-        array_push($FibonacciArr, $CurrentInt);
-        $TempInt = $CurrentInt + $NextInt;
+        array_push($FibonacciArr, $CurrentInt); //push $CurrentInt to the $FibonacciArr array
+        $TempInt = $CurrentInt + $NextInt; //calculate the next Fibonacci number
         $CurrentInt = $NextInt;
         $NextInt = $TempInt;
     }
@@ -14,10 +14,12 @@ function generateFibonacciSequence($LimitInt) {
     return $FibonacciArr;
 }
 
+//check if the 'limit' POST variable is set. If true, cast its value to an integer and store it in the $LimitInt variable.
 if (isset($_POST['limit'])) {
     $LimitInt = (int)$_POST['limit'];
     $FibonacciArr = generateFibonacciSequence($LimitInt);
 
+    //Iterate through the Fibonacci sequence stored in $FibonacciArr. For each iteration, output the current Fibonacci number 
     foreach ($FibonacciArr as $FibonacciNumberInt) {
         echo $FibonacciNumberInt . PHP_EOL;
     }
